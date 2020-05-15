a6da71f6d4c72fe053147d35cb5d04abaeb9a613
Skipp-it <mihairaducroitoru@gmail.com>
2020-05-15 12:51:07 +0300

    final project

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -19,13 +19,28 @@ ALTER TABLE IF EXISTS ONLY public.tag DROP CONSTRAINT IF EXISTS pk_tag_id CASCAD
 
 ALTER TABLE IF EXISTS ONLY public.question_tag DROP CONSTRAINT IF EXISTS fk_tag_id CASCADE;
 
+ALTER TABLE IF EXISTS ONLY public.users DROP CONSTRAINT IF EXISTS pk_id CASCADE;
 
+DROP TABLE IF EXISTS public.users CASCADE ;
+
+CREATE TABLE users (
+    id SERIAL NOT NULL,
+    username varchar(30) NOT NULL,
+    password varchar(500) NOT NULL,
+    submission_time timestamp without time zone,
+    count_questions int,
+    count_answers int,
+    count_comments int,
+    reputation int
+);
 
 DROP TABLE IF EXISTS public.question;
 
 CREATE TABLE question (
 
     id serial NOT NULL,
+
+    user_id int,
 
     submission_time timestamp without time zone,
 
@@ -49,6 +64,8 @@ CREATE TABLE answer (
 
     id serial NOT NULL,
 
+    user_id int,
+
     submission_time timestamp without time zone,
 
     vote_number integer,
@@ -57,7 +74,9 @@ CREATE TABLE answer (
 
     message text,
 
-    image text
+    image text,
+
+    valid boolean
 
 );
 
@@ -68,6 +87,8 @@ DROP TABLE IF EXISTS public.comment;
 CREATE TABLE comment (
 
     id serial NOT NULL,
+
+    user_id int,
 
     question_id integer,
 
@@ -108,6 +129,21 @@ CREATE TABLE tag (
 );
 
 
+ALTER TABLE ONLY users
+    ADD CONSTRAINT pk_id PRIMARY KEY (id);
+
+ALTER TABLE answer
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+ALTER TABLE question
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+ALTER TABLE comment
+ADD FOREIGN KEY (user_id) REFERENCES users(id);
+
+
 
 
 
@@ -146,6 +182,17 @@ ALTER TABLE ONLY comment
     ADD CONSTRAINT fk_answer_id FOREIGN KEY (answer_id) REFERENCES answer(id);
 
 
+ALTER TABLE ONLY answer
+
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+ALTER TABLE question
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+
+
+ALTER TABLE comment
+    ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
 
 ALTER TABLE ONLY answer
 
@@ -169,67 +216,69 @@ ALTER TABLE ONLY question_tag
 
     ADD CONSTRAINT fk_tag_id FOREIGN KEY (tag_id) REFERENCES tag(id);
 
-
-
-INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
-
-INSERT INTO question VALUES (1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
-
-
-
-I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine.
-
-
-
-BUT in my theme i also using jquery via webpack so the loading order is now following:
-
-
-
-jquery
-
-booklet
-
-app.js (bundled file with webpack, including jquery)', 'images/image1.png');
-
-INSERT INTO question VALUES (2, '2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
-
-', NULL);
-
-SELECT pg_catalog.setval('question_id_seq', 2, true);
-
-
-
-INSERT INTO answer VALUES (1, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
-
-INSERT INTO answer VALUES (2, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
-
-SELECT pg_catalog.setval('answer_id_seq', 2, true);
-
-
-
-INSERT INTO comment VALUES (1, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
-
-INSERT INTO comment VALUES (2, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
-
-SELECT pg_catalog.setval('comment_id_seq', 2, true);
-
-
-
-INSERT INTO tag VALUES (1, 'python');
-
-INSERT INTO tag VALUES (2, 'sql');
-
-INSERT INTO tag VALUES (3, 'css');
-
-SELECT pg_catalog.setval('tag_id_seq', 3, true);
+-- -- INSERT INTO users VALUES (0,'dan','dan','2017-04-28 08:29:00',1,1,2,1000);
+-- --
+-- -- INSERT INTO users VALUES (1,'ion','ion','2018-04-28 08:29:00',2,1,0,30);
+-- --
+-- -- INSERT INTO question VALUES (0,0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
+-- --
+-- -- INSERT INTO question VALUES (1,1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
+--
+--
+--
+-- I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine.
+--
+--
+--
+-- BUT in my theme i also using jquery via webpack so the loading order is now following:
+--
+--
+--
+-- jquery
+--
+-- booklet
+--
+-- app.js (bundled file with webpack, including jquery)', 'images/image1.png');
+--
+-- INSERT INTO question VALUES (2,1, '2017-05-01 10:41:00', 1364, 57, 'Drawing canvas with an image picked with Cordova Camera Plugin', 'I''m getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I''m on IOS, it throws errors such as cross origin issue, or that I''m trying to use an unknown format.
+--
+-- ', NULL);
+--
+-- SELECT pg_catalog.setval('question_id_seq', 2, true);
+--
+--
+--
+-- INSERT INTO answer VALUES (1,0, '2017-04-28 16:49:00', 4, 1, 'You need to use brackets: my_list = []', NULL);
+--
+-- INSERT INTO answer VALUES (2,1, '2017-04-25 14:42:00', 35, 1, 'Look it up in the Python docs', 'images/image2.jpg');
+--
+-- SELECT pg_catalog.setval('answer_id_seq', 2, true);
+--
+--
+--
+-- INSERT INTO comment VALUES (1,0, 0, NULL, 'Please clarify the question as it is too vague!', '2017-05-01 05:49:00');
+--
+-- INSERT INTO comment VALUES (2,0, NULL, 1, 'I think you could use my_list = list() as well.', '2017-05-02 16:55:00');
+--
+-- SELECT pg_catalog.setval('comment_id_seq', 2, true);
 
 
 
-INSERT INTO question_tag VALUES (0, 1);
-
-INSERT INTO question_tag VALUES (1, 3);
-
-INSERT INTO question_tag VALUES (2, 3);
+-- INSERT INTO tag VALUES (1, 'python');
+--
+-- INSERT INTO tag VALUES (2, 'sql');
+--
+-- INSERT INTO tag VALUES (3, 'css');
+--
+-- SELECT pg_catalog.setval('tag_id_seq', 3, true);
+--
+--
+--
+-- INSERT INTO question_tag VALUES (0, 1);
+--
+-- INSERT INTO question_tag VALUES (1, 3);
+--
+-- INSERT INTO question_tag VALUES (2, 3);
 
 
 
@@ -241,7 +290,16 @@ ALTER TABLE question_tag ADD CONSTRAINT fk_question_id
  REFERENCES question(id)
  ON DELETE CASCADE ON UPDATE CASCADE;
 
+ALTER TABLE question DROP CONSTRAINT fk_user_id;
+
+ALTER TABLE question ADD CONSTRAINT fk_user_id
+ FOREIGN KEY (user_id)
+ REFERENCES users(id)
+ ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 ALTER TABLE answer DROP CONSTRAINT fk_question_id;
+ALTER TABLE answer DROP CONSTRAINT fk_user_id;
 
 ALTER TABLE answer
  ADD CONSTRAINT fk_question_id
@@ -249,13 +307,28 @@ ALTER TABLE answer
  REFERENCES question(id)
  ON DELETE CASCADE ON UPDATE CASCADE;
 
+ALTER TABLE answer
+ ADD CONSTRAINT fk_user_id
+ FOREIGN KEY (user_id)
+ REFERENCES users(id)
+ ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 ALTER TABLE comment DROP CONSTRAINT fk_question_id;
+ALTER TABLE comment DROP CONSTRAINT fk_user_id;
 
 ALTER TABLE comment
  ADD CONSTRAINT fk_question_id
  FOREIGN KEY (question_id)
  REFERENCES question(id)
  ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE comment
+ ADD CONSTRAINT fk_user_id
+ FOREIGN KEY (user_id)
+ REFERENCES users(id)
+ ON DELETE CASCADE ON UPDATE CASCADE;
+
 
 ALTER TABLE question_tag DROP CONSTRAINT fk_tag_id;
 
@@ -265,7 +338,8 @@ ALTER TABLE question_tag DROP CONSTRAINT fk_tag_id;
  REFERENCES tag(id)
  ON DELETE CASCADE ON UPDATE CASCADE;
 
- ALTER TABLE comment DROP CONSTRAINT fk_answer_id;
+ALTER TABLE comment DROP CONSTRAINT fk_answer_id;
+ALTER TABLE comment DROP CONSTRAINT fk_user_id;
 
  ALTER TABLE comment
  ADD CONSTRAINT fk_answer_id
@@ -273,16 +347,5 @@ ALTER TABLE question_tag DROP CONSTRAINT fk_tag_id;
  REFERENCES answer(id)
  ON DELETE CASCADE ON UPDATE CASCADE;
 
-DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    id SERIAL NOT NULL,
-    username varchar(16) NOT NULL,
-    password varchar(500) NOT NULL,
-    submission_time timestamp without time zone,
-    count_questions int,
-    count_answers int,
-    count_comments int,
-    reputation int,
-    PRIMARY KEY (id)
-);
+
